796ff5d0c8900fed57dc3426a0ee7c75ebd93016
Andrew <54780326+echo-upsurge@users.noreply.github.com>
2022-10-15 22:02:26 -0500

    Fixed flight code

--- tello.py
+++ tello.py
@@ -1,7 +1,7 @@
 import time
 from djitellopy import Tello
 import cv2
-from simple_pid import PID
+import simple_pid
 import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
@@ -14,11 +14,11 @@ cap = cv2.VideoCapture(1)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
-initialize = False
+initialize = True
 
 me = Tello()
 me.connect()
-me.for_back_velocity = 0
+me.forward_backward_velocity = 0
 me.left_right_velocity = 0
 me.up_down_velocity = 0
 me.yaw_velocity = 0
@@ -58,28 +58,35 @@ while True:
                                     )      
 
         cv2.imshow("MyResult", image)
-
+    centerX = 0
+    centerY = 0
+    visibilityLeft = 0
     try:
         landmarks = results.pose_landmarks.landmark
-        
+        centerX = (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x + landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x)/2
+        centerY = (landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y + landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y)/2
+        visibilityLeft = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility
     except:
         pass
- 
-    if initialize:
-        me.takeoff()
-        time.sleep(6)
-        me.rotate_counter_clockwise(90)
-        time.sleep(3)
-        me.rotate_clockwise(90)
-        me.land()
-        initialize = False
     
-    print(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value])
-    print(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value])
+    print(visibilityLeft)
+
+    # if initialize:
+    #     me.takeoff()
+    #     initialize = False
+
+    # me.yaw_velocity = 99
+
+    if me.send_rc_control:
+        me.send_rc_control(me.left_right_velocity, me.forward_backward_velocity, me.up_down_velocity, 99)
+
+    # if me.get_battery() < 40:
+    #     me.land()
+
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         me.land()
         cv2.destroyAllWindows()
         break
-exit()
+
         
